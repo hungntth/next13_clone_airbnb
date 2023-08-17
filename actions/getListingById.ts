5f6bc237b76c19e144fb115fa9f07fc6271101ed
HungNt <hungnt@techasians.com>
2023-08-17 14:40:12 +0700

    fix bug

--- actions/getListingById.ts
+++ actions/getListingById.ts
@@ -1,18 +1,22 @@
-interface IPrams {
+import prisma from "@/libs/prismadb";
+
+interface IParams {
   listingId?: string;
 }
 
-export default async function getListingById(params: IPrams) {
+export default async function getListingById(
+  params: IParams
+) {
   try {
     const { listingId } = params;
 
-    const listing = await prisma?.listing.findUnique({
+    const listing = await prisma.listing.findUnique({
       where: {
         id: listingId,
       },
       include: {
-        user: true,
-      },
+        user: true
+      }
     });
 
     if (!listing) {
@@ -21,13 +25,14 @@ export default async function getListingById(params: IPrams) {
 
     return {
       ...listing,
-      createdAt: listing.createdAt.toISOString(),
+      createdAt: listing.createdAt.toString(),
       user: {
         ...listing.user,
-        createdAt: listing.user.createdAt.toISOString(),
-        updatedAt: listing.user.updatedAt.toISOString(),
-        emailVerified: listing.user.emailVerified?.toISOString() || null,
-      },
+        createdAt: listing.user.createdAt.toString(),
+        updatedAt: listing.user.updatedAt.toString(),
+        emailVerified: 
+          listing.user.emailVerified?.toString() || null,
+      }
     };
   } catch (error: any) {
     throw new Error(error);
